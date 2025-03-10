70db1bfd8f23e9f5bdbe819dea5a1c049978f463
kristof <csorba.kristof@vik.bme.hu>
2025-03-10 11:05:45 +0100

    TemperatureMonitoring csv and model added, 50p

---
+++ TemperatureMonitoring/TemperatureMonitoring/Model/TempHum.cs
@@ -0,0 +1,11 @@
+﻿namespace TemperatureMonitoring.Model
+{
+    public class TempHum
+    {
+        public required string DeviceName { get; set; }
+        public long TimeStamp { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
