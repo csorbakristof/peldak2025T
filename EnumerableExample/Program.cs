7635e5c2d5816c9b5b0e2bc1301269ab2a26da9d
kristof <csorba.kristof@vik.bme.hu>
2025-03-10 10:41:21 +0100

    Enumerable example 1, 25p

---
+++ EnumerableExample/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EnumerableExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Values:");
+            // LINQ
+            // Lamda expression: v => v > 2
+            foreach (var v in GetValues().Where( v => v > 1 )
+                .Select( v => 3*v ))
+                Console.WriteLine(v);
+        }
+
+        public static IEnumerable<int> GetValues()
+        {
+            var tomb = new int[] { 1, 2, 3, 4, 5 };
+            return tomb;
+        }
+    }
+}
