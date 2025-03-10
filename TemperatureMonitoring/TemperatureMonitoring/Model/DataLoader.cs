6f207f05a350ff7304c8fc8abb8498a368e917f5
kristof <csorba.kristof@vik.bme.hu>
2025-03-10 11:11:30 +0100

    DataLoader added, 56p

---
+++ TemperatureMonitoring/TemperatureMonitoring/Model/DataLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureMonitoring.Model
+{
+    public class DataLoader
+    {
+        public IEnumerable<TempHum> LoadCsv(string filename)
+        {
+            var lines = System.IO.File.ReadAllLines(filename);
+            string deviceName = lines[0];
+            for (int i = 2; i < lines.Length; i++)
+            {
+                var fields = lines[i].Split(';');
+                var timestamp = long.Parse(fields[0]);
+                yield return new TempHum
+                {
+                    DeviceName = deviceName,
+                    TimeStamp = timestamp,
+                    Temperature = double.Parse(fields[1]),
+                    Humidity = double.Parse(fields[2]),
+                    Time = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(timestamp)
+                };
+            }
+        }
+    }
+}
