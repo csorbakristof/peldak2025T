9a186980983f432fe1dc06d969401df3000eb4d3
kristof <csorba.kristof@vik.bme.hu>
2025-03-10 10:49:14 +0100

    yield return, 34p

--- EnumerableExample/Program.cs
+++ EnumerableExample/Program.cs
@@ -7,15 +7,19 @@
             Console.WriteLine("Values:");
             // LINQ
             // Lamda expression: v => v > 2
-            foreach (var v in GetValues().Where( v => v > 1 )
-                .Select( v => 3*v ))
+            foreach (var v in GetValues())
                 Console.WriteLine(v);
         }
 
         public static IEnumerable<int> GetValues()
         {
+            // Valahogyan lesznek adataink...
             var tomb = new int[] { 1, 2, 3, 4, 5 };
-            return tomb;
+            // Egyesével adjuk vissza őket
+            foreach (var v in tomb)
+            {
+                yield return v;
+            }
         }
     }
 }
