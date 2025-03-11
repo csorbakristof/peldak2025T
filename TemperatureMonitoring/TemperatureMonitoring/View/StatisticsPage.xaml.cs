3e2762080f18afebd9409ae8d1c25a1363499a59
kristof <csorba.kristof@vik.bme.hu>
2025-03-11 11:55:11 +0100

    OxyPlot initialized

--- TemperatureMonitoring/TemperatureMonitoring/View/StatisticsPage.xaml.cs
+++ TemperatureMonitoring/TemperatureMonitoring/View/StatisticsPage.xaml.cs
@@ -1,3 +1,6 @@
+using OxyPlot.Series;
+using OxyPlot;
+
 namespace TemperatureMonitoring.View;
 
 public partial class StatisticsPage : ContentPage
@@ -5,5 +8,19 @@ public partial class StatisticsPage : ContentPage
 	public StatisticsPage()
 	{
 		InitializeComponent();
-	}
+		this.BindingContext = this;
+
+
+        var dataPoints = Enumerable.Range(1, 10).Select(x => new DataPoint(x, x * 10));
+        var model = new PlotModel();
+        model.Title = "Sample Test";
+        model.Series.Add(new LineSeries
+        {
+            ItemsSource = dataPoints,
+            Color = OxyColors.Blue,
+            MarkerType = MarkerType.Square,
+        });
+        PlotView.Model = model;
+        model.InvalidatePlot(true);
+    }
 }
