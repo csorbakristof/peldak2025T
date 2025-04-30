7ddfa5158914f1a44c9ffe70780e61cd104aebf6
kristof <csorba.kristof@vik.bme.hu>
2025-04-30 15:35:14 +0200

    etap 5 - statistic added (daily diff)

---
+++ TemperatureMonitoring/TemperatureMonitoring/ViewModel/DailyValue.cs
@@ -0,0 +1,8 @@
+﻿namespace TemperatureMonitoring.ViewModel
+{
+    public class DailyValue
+    {
+        public DateOnly Day { get; set; }
+        public double Value { get; set; }
+    }
+}
--- TemperatureMonitoring/TemperatureMonitoring/ViewModel/TempHumViewModel.cs
+++ TemperatureMonitoring/TemperatureMonitoring/ViewModel/TempHumViewModel.cs
@@ -19,14 +19,56 @@ namespace TemperatureMonitoring.ViewModel
 
         private void UpdateStatistics()
         {
+            UpdateTempHum200();
+            UpdateDailyMeanExternalInternalDifference();
+        }
+
+        private void UpdateTempHum200()
+        {
             TempHum200List.Clear();
-            foreach(var item in rawTempHumContainer.AllTempHums.Take(200))
-            {
+            foreach (var item in rawTempHumContainer.AllTempHums.Take(200))
                 TempHum200List.Add(item);
-            }
+        }
+
+        private const string ExternalTemperatureDeviceName = "T2_Terasz";
+        private const string InternalTemperatureDeviceName = "T3_Kek";
+
+        private void UpdateDailyMeanExternalInternalDifference()
+        {
+            var externalMeans = GetDailyMeans(rawTempHumContainer.AllTempHums
+                .Where(th => th.DeviceName == ExternalTemperatureDeviceName));
+            var internalMeans = GetDailyMeans(rawTempHumContainer.AllTempHums
+                .Where(th => th.DeviceName == InternalTemperatureDeviceName));
+
+            // Create a list with the difference between the external and internal means for every day separately.
+            var dailyDifferences = externalMeans
+                .Join(internalMeans,
+                    e => e.Day,
+                    i => i.Day,
+                    (e, i) => new DailyValue
+                    {
+                        Day = e.Day,
+                        Value = e.Value - i.Value
+                    });
+
+            DailyMeanExternalInternalDifference.Clear();
+            foreach (var item in dailyDifferences)
+                DailyMeanExternalInternalDifference.Add(item);
+        }
+
+        private IEnumerable<DailyValue> GetDailyMeans(IEnumerable<TempHum> tempHums)
+        {
+            return tempHums.GroupBy(th => th.Time.Date)
+                .Select(g => new DailyValue {
+                    Day = new DateOnly(g.Key.Year, g.Key.Month, g.Key.Day),
+                    Value = g.Average(th => th.Temperature)
+                });
         }
 
         public ObservableCollection<TempHum> TempHum200List { get; set; }
             = new ObservableCollection<TempHum>();
+
+        public ObservableCollection<DailyValue> DailyMeanExternalInternalDifference { get; set; }
+            = new ObservableCollection<DailyValue>();
     }
 }
